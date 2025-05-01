caaad7e4d9aa6063b744610c6929bf86aed37c2c
Vitor Hugo <vhaf22@gmail.com>
2025-05-01 20:39:31 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#creating a class to not have to deal with global and local differences
+class cookie_clicker:
+    def __init__(self):
+        self.set_initial_variables()
+        self.initialize()
+    
+    #function to initialize variables
+    def set_initial_variables(self)->None:
+        self.max_time = 0
+        self.cookies = 0
+        self.cursors = 0
+        self.up_cursors = 0
+        self.grandmas = 0
+        self.up_grandmas = 0
+
+    #function to initialize the game
+    def initialize(self)->None:
+        self.max_time = int(input("Enter the time in seconds to run the script: "))
+        bakery_name = str(input("Enter the bakers name: "))
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://orteil.dashnet.org/cookieclicker/")
+        self.driver.maximize_window()
+        language_selected = False
+        while not language_selected:
+            try:
+                languague_selection = self.driver.find_element(By.ID, 'langSelect-PT-BR')
+                languague_selection.click()
+                language_selected = True
+            except:
+                pass
+        bakery_named = False
+        while not bakery_named:
+            try:
+                bakery_selection_button = self.driver.find_element(By.ID, 'bakeryName')
+                bakery_selection_button.click()
+                time.sleep(0.5)
+                bakery_selection = self.driver.find_element(By.ID, 'bakeryNameInput')
+                bakery_selection.send_keys(bakery_name)
+                (self.driver.find_element(By.ID, 'promptOption0')).click()
+                bakery_named = True
+            except:
+                pass
+        (self.driver.find_element(By.CLASS_NAME, 'cc_btn_accept_all')).click()
+        self.cookie = self.driver.find_element(By.ID, 'bigCookie')
+    
+    #function to buy cursors when cookies>=15*(1.15^cursors) -> math function by which the price is calculated
+    def buy_cursors(self)->None:
+        if self.cookies>=round(15*((1.15)**self.cursors)):
+            product0 = self.driver.find_element(By.ID, 'product0')
+            product0.click()
+            self.cursors+=1
+    
+    #function to buy grandmas when cookies>=100*(1.15^grandmas) -> math function by which the price is calculated        
+    def buy_grandma(self)->None:
+        if self.cookies>=round(100*((1.15)**self.grandmas)):
+            product1 = self.driver.find_element(By.ID, 'product1')
+            product1.click()
+            self.grandmas+=1
+    
+    #function to buy the first upgrade available        
+    def buy_upgrade(self)->bool:
+        try:
+            upgrade = self.driver.find_element(By.CSS_SELECTOR, '.upgrade.enabled')       
+            upgrade.click()
+            return True
+        except:
+            return False
+        
+    def play(self)->None:
+        start = time.time()
+        while(time.time()-start<self.max_time):
+            try:
+                golden = self.driver.find_element(By.CSS_SELECTOR, '.shimmer')
+                golden.click()
+            except:
+                pass
+            self.cookie.click()
+            self.cookies = int(self.driver.find_element(By.ID, 'cookies').text.split()[0].replace(',', ''))
+            if self.up_cursors==0:
+                if self.cursors==0:
+                    self.buy_cursors()
+                elif self.buy_upgrade():
+                    self.up_cursors+=1
+            elif self.cursors<=5:
+                self.buy_cursors()
+            elif self.up_cursors<2:
+                if self.buy_upgrade():
+                    self.up_cursors+=1
+            elif self.grandmas==0:
+                self.buy_grandma()
+            elif self.up_grandmas==0:
+                if self.buy_upgrade():
+                    self.up_grandmas+=1
+            elif self.up_cursors>=2 and self.grandmas<=3 and self.up_grandmas>0:
+                self.buy_grandma()
+            else:
+                if self.cursors<=10:
+                    self.buy_cursors()
+                elif self.grandmas<=5:
+                    self.buy_grandma()
+                elif self.buy_upgrade():
+                    self.up_grandmas+=1
+    def get_stats(self)->None:
+        stats_button = self.driver.find_element(By.ID, 'statsButton')
+        stats_button.click()
+        information = self.driver.find_elements(By.CLASS_NAME, 'listing')
+        print(information[1].text)
+    def quit_game(self)->None:
+        self.driver.quit()
+        print("Game closed")
+        
+if __name__ == '__main__':
+    bot = cookie_clicker()
+    bot.play()
+    bot.get_stats()
+    bot.quit_game()
